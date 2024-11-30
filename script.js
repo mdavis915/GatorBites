9f5795727bd11a94c123c8d493d26c6199bf6725
Blobs0111 <148994506+Blobs0111@users.noreply.github.com>
2024-11-30 18:37:47 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// When submit button is clicked, gets text from the textbox and "prints" it out
+subButton.addEventListener("click", function(event) {
+  input = document.getElementById("ingredients").value;
+  console.log(input);  
+});  
